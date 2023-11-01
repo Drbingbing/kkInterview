9a5a933fb7d84ff76c7e47421fcc4c88bd739562
Drbingbing <tw718140@gmail.com>
2023-11-01 23:26:26 +0800

    add new scheme for native ui

---
+++ KKInterview2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KKInterview2
+//
+//  Created by Bing Bing on 2023/11/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
